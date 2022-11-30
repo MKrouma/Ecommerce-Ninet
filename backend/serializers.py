b9f5163fc52e8c252f8990b969ebdbcc46c79f4e
mkrouma <mamadoukrouma@outlook.com>
2022-11-30 08:20:02 +0000

    doc - serializer without image

--- backend/serializers.py
+++ backend/serializers.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id' ,'name', 'description', 'price', 'img_file')
+        fields = ('id' ,'name', 'description', 'price')
