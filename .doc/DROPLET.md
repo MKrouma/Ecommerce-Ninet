0e3aaa935bcd3d59ba2f777ed2127922373f6a28
mkrouma <mamadoukrouma@outlook.com>
2022-12-01 17:11:56 +0000

    FIX : CRSF issue

--- .doc/DROPLET.md
+++ .doc/DROPLET.md
@@ -3,6 +3,7 @@ Droplet is a linux virtual machine host on Digital Ocean.
 [Connect to droplet with OpenSSH](https://docs.digitalocean.com/products/droplets/how-to/connect-with-ssh/openssh/)
 [Create a firewell](https://docs.digitalocean.com/products/networking/firewalls/quickstart/)
 [Set Postgres database](https://docs.digitalocean.com/products/databases/postgresql/quickstart/)
+
 ## set ssh
 ssh -i ~/.ssh/id_rsa_2 root@ADDRES_IP
 ssh -i ~/.ssh/id_rsa_2 mahadou@ADDRES_IP
--- backend/settings.py
+++ backend/settings.py
@@ -139,8 +139,10 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     'https://*.mydomain.com',
     'https://*.127.0.0.1'
-    'https://frontend_react'
+    'https://frontend_react',
+    'http://localhost:3000'
 ]
+CSRF_COOKIE_SECURE = False
 
 
 PUBLIC_MEDIA_LOCATION = "media"
@@ -164,12 +166,6 @@ CACHES = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "ninetshoppingstorage"
@@ -178,27 +174,12 @@ AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
      "ACL": "public-read"
 }
-# AWS_LOCATION = "https://ninetshoppingstorage.fra1.digitaloceanspaces.com"
 AWS_LOCATION = "static"
-
-# AWS_ACCESS_KEY_ID = 'your-spaces-access-key'
-# AWS_SECRET_ACCESS_KEY = 'your-spaces-secret-access-key'
-# AWS_STORAGE_BUCKET_NAME = 'your-storage-bucket-name'
-# AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
-# AWS_LOCATION = 'your-spaces-files-folder'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles-cdn" # dev example
-# from .cdn.conf import *  # noqa
-# STATICFILES_STORAGE = 'https://ninetshoppingstorage.fra1.digitaloceanspaces.com/'
 
 # Logging
 # Define our to log from server
