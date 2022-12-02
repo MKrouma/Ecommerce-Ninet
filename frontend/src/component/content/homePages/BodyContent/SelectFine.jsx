4ece75692211f3ca8b151a9796c47fb2218e1526
unknown <doumbiavamoussadev@gmail.com>
2022-12-03 00:45:29 +0100

    navLing position resolue

--- frontend/src/component/content/homePages/BodyContent/SelectFine.jsx
+++ frontend/src/component/content/homePages/BodyContent/SelectFine.jsx
@@ -4,8 +4,16 @@ function SelectFine() {
     <>
         <div className="containerSelect">
             <div className="contentLeft-select">
+                <div className="contentProduit-img">
+
+                </div>
                 <div className="imgSelect">
+                    
+                </div>
+                <div className="contentDiplay-iterm">
                     <div className="infoProduit-1">
+                    
+                        <div>
                         <div className="contentProduit-info">
                             <div className="title--produit">
                                 <p>Peau</p>
@@ -27,35 +35,51 @@ function SelectFine() {
                             
                         </div>
                     </div>
-                </div>
-                
-            </div>
-            <div className="contentRigth-select">
-                <div className="imgSelect-2">
-                    <div className="infoProduit-2">
-                        <div className="contentProduit-info">
-                            <div className="title--produit">
-                                <p>Peau</p>
-                            </div>
-                            <div className="produit--description">
-                                <p>Masque capillaire Restore</p>
-                            </div>
-                            <div className="produit--description-sh">
-                                <p>Harmessing the growing power of onion</p>
-                            </div>
-                            <dir className="produit---price">
-                                <p>26.000 xof</p> 
-                            </dir>
-                        </div>
-                        <div className="btn-produit">
-                            <div className="content-btn-produits">
-                                <button>Ajouter au panier</button>
-                            </div>
-                            
-                        </div>
+                    
+                    
                         
                     </div>
                 </div>
+            </div>
+            <div className="contentRigth-select">
+                 <div className="contentLeft-select">
+                <div className="contentProduit-img">
+
+                </div>
+                <div className="imgSelect-2">
+                    
+                </div>
+                <div className="contentDiplay-iterm">
+                    <div className="infoProduit-1">
+                    
+                        <div>
+                        <div className="contentProduit-info">
+                            <div className="title--produit">
+                                <p>Peau</p>
+                            </div>
+                            <div className="produit--description">
+                                <p>Masque capillaire Restore</p>
+                            </div>
+                            <div className="produit--description-sh">
+                                <p>Harmessing the growing power of onion</p>
+                            </div>
+                            <dir className="produit---price">
+                                <p>26.000 xof</p> 
+                            </dir>
+                        </div>
+                        <div className="btn-produit">
+                            <div className="content-btn-produits">
+                                <button>Ajouter au panier</button>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                    
+                        
+                    </div>
+                </div>
+            </div>
                 
             </div>
         </div>
